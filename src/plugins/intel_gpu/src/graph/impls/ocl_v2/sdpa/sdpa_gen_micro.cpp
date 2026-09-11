fb3bc36d2e864bc07d71acc05d0023cff81a1076
hyunback <hyunback.kim@intel.com>
2026-09-11 11:43:23 +0900

    Fix CL_BUILD_FAILURE error
    This is temp fixing from the latest onednn with OV sync issue in micro_sdpa
    char/uchar to s4/u4
    
    Signed-off-by: hyunback <hyunback.kim@intel.com>

--- src/plugins/intel_gpu/src/graph/impls/ocl_v2/sdpa/sdpa_gen_micro.cpp
+++ src/plugins/intel_gpu/src/graph/impls/ocl_v2/sdpa/sdpa_gen_micro.cpp
@@ -1033,7 +1033,9 @@ KernelData SDPAMicroGenerator::get_kernel_data(const kernel_impl_params& params)
     kd.code = std::make_shared<KernelString>();
     kd.code->language = kernel_language::OCLC_V2;
     kd.code->entry_point = get_entry_point(params);
-    kd.code->jit = "";
+    // oneDNN 0c5ff275bf emits custom type names for sub-byte types in ugemm prototypes.
+    // OV passes the packed KV buffers as uchar, so alias them to the matching 1-byte storage type.
+    kd.code->jit = "#define u4 uchar\n#define s4 char\n";
     kd.code->undefs = "";
     kd.code->options = get_build_options(params);
     kd.code->batch_compilation = false;
